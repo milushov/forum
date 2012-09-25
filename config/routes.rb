b4dd1e12b309de7d60e632af93bdd7a70f7f1182
Roman Milushov <github@milushov.ru>
2012-09-25 23:52:12 +0400

    topic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Forum::Application.routes.draw do
+  resources :topics
+
   resources :boards
 
   devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120925194445) do
+ActiveRecord::Schema.define(:version => 20120925195054) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "topics", :force => true do |t|
+    t.string   "subject"
+    t.string   "text"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
