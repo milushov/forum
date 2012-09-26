d9848636e643fb81365760db23a3b6b0b0cc6b4b
Roman Milushov <github@milushov.ru>
2012-09-26 15:47:33 +0400

    setup relations between board and topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
-  attr_accessible :subject, :text
+  attr_accessible :subject
+
+  belongs_to :board, foreign_key: 'board_id'
+
+  default_scope order: 'created_at'
 end
