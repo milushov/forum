16f1288951c86a928a25c34cb7f7a799c41a419c
Roman Milushov <github@milushov.ru>
2012-09-27 19:28:19 +0400

    user-role

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   validates_presence_of :name
 
   has_many :posts
+  has_and_belongs_to_many :roles
 
   default_scope order: 'created_at'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120927152045) do
+ActiveRecord::Schema.define(:version => 20120927152944) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(:version => 20120927152045) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
   end
 
   create_table "topics", :force => true do |t|
