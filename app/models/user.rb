edd14d8d2ffa3ec93785539d3e4bf5995778b686
Roman Milushov <github@milushov.ru>
2012-09-27 19:41:42 +0400

    changed abiliry.rb and added role? method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
 
   default_scope order: 'created_at'
+
+  def role?(role)
+    return !!self.roles.find_by_name(role.to_s.camelize)
+  end
 end
