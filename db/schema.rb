661477da93628a80535de71d8d7cf6fbe1d1d07b
Roman Milushov <github@milushov.ru>
2012-09-27 19:27:34 +0400

    role model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120926141641) do
+ActiveRecord::Schema.define(:version => 20120927152045) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(:version => 20120926141641) do
     t.datetime "updated_at", :null => false
     t.integer  "topic_id"
     t.integer  "user_id"
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "topics", :force => true do |t|
