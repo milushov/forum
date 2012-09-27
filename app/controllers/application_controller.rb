c52537b13158a15339e55fb1db5e6c7567c404d6
Roman Milushov <github@milushov.ru>
2012-09-27 22:00:11 +0400

    authorizing on each action of all resources by 1 line!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  load_and_authorize_resource
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
