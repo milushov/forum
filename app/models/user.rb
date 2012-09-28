07ee80153f129047664989fab7fcdfe6601556e7
Roman Milushov <github@milushov.ru>
2012-09-28 11:53:00 +0400

    update model

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,15 @@ class User < ActiveRecord::Base
       user.name = auth.info.nickname
     end
   end
+
+  def self.new_with_session(params, session)
+    if session['devise.user_attributes']
+      new(session['devise.user_attributes'], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
 end
