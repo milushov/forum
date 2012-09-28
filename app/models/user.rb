235fc659703238398e6eeecd0aec5786ea8b94b7
Roman Milushov <github@milushov.ru>
2012-09-28 05:56:57 +0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,12 @@ class User < ActiveRecord::Base
   def admin?
     role? :admin
   end
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.nickname
+    end
+  end
 end
