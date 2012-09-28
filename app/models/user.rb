504d0a184cd5a25631ef496eb31946fe30710cc4
Roman Milushov <github@milushov.ru>
2012-09-28 13:26:35 +0400

    fix saving avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.nickname
-      user.avatar = auth.info.image
+      user.avatar = auth.info.image.sub('_normal', '')
     end
   end
 
